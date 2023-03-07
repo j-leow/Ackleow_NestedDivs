ff02fe9dfb6a19c6d21624ee658fb8e602434dd9
Jules Ackleow <jleow@iu.edu>
2023-03-07 22:03:48 +0000

    Moved project to VS Code for easier editing.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
+      {/* //Create an outer div to encapsulate 4 divs. The fourth div will have
+      another div nested within it. */}
       <div>
         <div></div>
         <div></div>
